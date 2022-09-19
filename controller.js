43d219958aab81be0f2f417c668034851c685bd8
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2022-09-19 01:41:01 -0400

    Added a modal for the game rules

--- controller.js
+++ controller.js
@@ -9,3 +9,31 @@ window.onload = () => {
   hamburger.addEventListener('click', () => toggle(navMenu, 'is-active'));
   Array.from(navItems).forEach(e => e.addEventListener('click', toggleNav));
 }
+
+///// modals /////
+// Get the modal
+var modal = document.getElementById("myModal");
+var modal = document.getElementById("zombie-directions");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
